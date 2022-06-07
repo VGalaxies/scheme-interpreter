d72c8cc69965f3ba5ed38a2a2424d6674a4e7f02
V_Galaxy <1904821183@qq.com>
2022-06-07 16:54:35 +0800

    update Problem 4 with doubt

--- scheme_forms.py
+++ scheme_forms.py
@@ -38,6 +38,11 @@ def do_define_form(expressions, env):
         validate_form(expressions, 2, 2)  # Checks that expressions is a list of length exactly 2
         # BEGIN PROBLEM 4
         "*** YOUR CODE HERE ***"
+        first, rest = expressions.first, expressions.rest
+        assert rest.rest == nil
+        res = scheme_eval(rest.first, env)
+        env.define(first, res)
+        return first
         # END PROBLEM 4
     elif isinstance(signature, Pair) and scheme_symbolp(signature.first):
         # defining a named procedure e.g. (define (f x y) (+ x y))
